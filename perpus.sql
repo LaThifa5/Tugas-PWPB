e842f7f40044a5a08b0a5246f35f65e1ce389a90
Lathifa Nurliani <70360822+LaThifa5@users.noreply.github.com>
2021-04-30 21:21:06 +0700

    Add files via upload

---
+++ perpus.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 30 Apr 2021 pada 09.35
+-- Versi Server: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `anggota`
+--
+
+CREATE TABLE `anggota` (
+  `id_anggota` int(3) NOT NULL,
+  `nm_anggota` varchar(32) DEFAULT NULL,
+  `alamat` text,
+  `ttl_anggota` text,
+  `status_anggota` varchar(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `anggota`
+--
+
+INSERT INTO `anggota` (`id_anggota`, `nm_anggota`, `alamat`, `ttl_anggota`, `status_anggota`) VALUES
+(111, 'rani', 'bandung', 'bandung25juni2003', '1'),
+(222, 'lathifa', 'bsi', 'bandung, 5 april 2003', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `judul_buku` varchar(32) DEFAULT NULL,
+  `pengarang` varchar(32) DEFAULT NULL,
+  `jenis_buku` varchar(32) DEFAULT NULL,
+  `penerbit` varchar(32) DEFAULT NULL,
+  `kd_buku` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `meminjam`
+--
+
+CREATE TABLE `meminjam` (
+  `id_pinjam` int(3) NOT NULL,
+  `tgl_pinjam` date DEFAULT NULL,
+  `jumlah_pinjam` int(2) DEFAULT NULL,
+  `tgl_kembali` date DEFAULT NULL,
+  `id_anggota` int(3) DEFAULT NULL,
+  `kd_buku` varchar(5) DEFAULT NULL,
+  `kembali` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`id_anggota`);
+
+--
+-- Indexes for table `meminjam`
+--
+ALTER TABLE `meminjam`
+  ADD PRIMARY KEY (`id_pinjam`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
